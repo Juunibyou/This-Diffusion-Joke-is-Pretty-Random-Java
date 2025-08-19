5c67f058a5555212606588fc5df5b2c46350e50a
Juunibyou <danijunn@gmail.com>
2025-08-19 11:45:39 -0500

    Beginning new lab
    added a random generator that would work as a coin flip. I hope.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,6 +3,6 @@ package org.example;
 public class App {
     public static void main(String[] args) {
       GreetingGenerator generator = new GreetingGenerator();
-      System.out.println(generator.sayHello());
+      
     }
 }
--- app/src/main/java/org/example/GreetingGenerator.java
+++ app/src/main/java/org/example/GreetingGenerator.java
@@ -1,10 +1,19 @@
 package org.example;
+import java.util.Random;
 
 public class GreetingGenerator {
   // This is just an example class to demonstrate how everything works
   // You should delete this file when you implement your own solution
 
-  public String sayHello() {
-    return "Hello World!";
+  //N = number of steps = 10
+  //M = number of trial = 10
+  //D = Final Position
+
+
+  public int Coinflip() {
+    Random randomGenerator = new Random();
+    int randomNumber = randomGenerator.nextInt(2);
+
+    return randomNumber;
   }
 }
